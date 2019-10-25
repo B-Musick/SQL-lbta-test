af3b9fdf5ad6949c150e9bb27f01272babc9d357
B-Musick <bmuze1@gmail.com>
2019-10-25 14:10:26 -0600

    Update route finished and works

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var app     = express();
 // Useful to figure out where went wrong in the routes
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+app.set('view engine','ejs');
 
 
 app.use(express.static('./public')); // App server is now serving all the files in this folder, just use root
@@ -11,8 +13,10 @@ app.use(morgan('short'));
 var mysql = require('mysql');
 
 // Middleware to help process requests, it can go in POST request and retrieve data
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({extended: false}));
 
+// METHOD-OVERRIDE (PUT form action) ?_method=PUT
+app.use(methodOverride('_method'));
 function getConnection(){
     return mysql.createConnection({
         host: "localhost",
@@ -30,7 +34,7 @@ function getConnection(){
 //         console.log("Database created");
 //     });
 // });
-app.get('/user', (req, res) => {
+app.get('/users', (req, res) => {
     console.log('Fetching user with id: ' + req.params.id);
 
     // Connect to database
@@ -49,10 +53,10 @@ app.get('/user', (req, res) => {
 
         // Rename the values from SQL for JSON
         const users = rows.map(row => {
-            return { firstName: row.first_name }
+            return { firstName: row.first_name, lastName: row.last_name, id:row.id }
         });
 
-        res.json(rows);
+        res.render('users', {users});
     })
 });
 app.get('/user/:id', (req, res) => {
@@ -62,6 +66,7 @@ app.get('/user/:id', (req, res) => {
     var con = getConnection();
 
     const userId = req.params.id;
+    
     const queryString = "SELECT * FROM users WHERE id = ?";
     con.query(queryString, [userId], (err, rows, fields) => {
         if(err) {
@@ -74,10 +79,10 @@ app.get('/user/:id', (req, res) => {
 
         // Rename the values from SQL for JSON
         const users = rows.map(row=>{
-            return {firstName: row.first_name}
+            return { firstName: row.first_name, lastName: row.last_name, id: row.id}
         });
 
-        res.json(rows);
+        res.json(users);
     })
     
 });
@@ -93,6 +98,7 @@ app.post('/user',(req,res)=>{
     let lastName = req.body.last_name;
 
     let queryString = 'INSERT INTO users (first_name, last_name) VALUES (?,?)'
+    // The values in the brackets are inserted into the query string (?,?)
     getConnection().query(queryString,[firstName,lastName],(err,results,fields)=>{
         if(err){
             console.log('Failed to input new user');
@@ -104,11 +110,55 @@ app.post('/user',(req,res)=>{
     })
     
     
+});
+
+app.get('/user/:id/edit',(req,res)=>{
+    var con = getConnection();
+
+    const userId = req.params.id;
+
+    const queryString = "SELECT * FROM users WHERE id = ?";
+    con.query(queryString, [userId], (err, rows, fields) => {
+        if (err) {
+            console.log('failed to query users' + err)
+            res.sendStatus(500);
+            // throw err (can do this istead)
+            res.end()
+            return
+        }
+        
+  
+        // rows returns [{user}] so need to take it out of the array
+        res.render('update',{user:rows[0]});
+    });
+    
+})
+
+app.put('/user/:id',(req,res)=>{
+    
+    let userId = req.params.id;
+    let firstName = req.body.first_name;
+    let lastName = req.body.last_name;
+    console.log(userId);
+    let queryString = 'UPDATE users SET first_name= ?, last_name= ? WHERE id= ?';
+    // The values in the brackets are inserted into the query string (?,?)
+    getConnection().query(queryString, [firstName, lastName,userId], (err, results, fields) => {
+        if (err) {
+            console.log('Failed to update user');
+            res.sendStatus(500);
+            return
+        }
+        console.log('Updated a user ' + results);
+        res.redirect('/users');
+    })
+//     let firstName = req.params.first_name;
+//     let lastName = req.params.last_name;
+//     let userId = req.params.id;
+//     console.log(req.body);
+//     res.render('update',{userId, firstName,lastName});
 })
 
 app.listen(3002,()=>{
     console.log('Server is running and listening on 3002...');
 })
 
-// CONTINUE
-// https://www.youtube.com/watch?v=1qH-3SIFmXw&list=PL0dzCUj1L5JE4w_OctDGyZOhML6OtJSqR&index=3
